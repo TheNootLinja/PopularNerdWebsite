d3f5d51576fd3391772908fa9042576940162f02
TheNootLinja <thenootlinja@gmail.com>
2020-11-18 13:22:17 -0500

    Inital commit of project start

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Header from './UI/Header.svelte';
+</script>
+
+<style>
+  main {
+    color: #1b2d45;
+    text-align: center;
+    padding: 1em;
+    /* max-width: 240px; */
+    margin: 0 auto;
+    height: 300rem;
+  }
+
+  h1 {
+    color: #1b2d45;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
+
+<Header />
+<main>
+  <h1>My name is Nicholas Peters</h1>
+  <p>I am a full-stack developer</p>
+</main>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,48 @@
+<script>
+</script>
+
+<style>
+  header {
+    color: white;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 3rem;
+    background-color: #333;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  header nav a {
+    color: white;
+    text-decoration: none;
+    border-bottom: solid 2px #00ebc7;
+  }
+
+  header nav a + a {
+    margin-left: 10px;
+  }
+
+  .last {
+    margin-right: 2rem;
+  }
+
+  .fake-logo {
+    font-size: 120%;
+    margin: 0;
+    margin-left: 2rem;
+    padding-right: 6px;
+  }
+</style>
+
+<header>
+  <h1 class="fake-logo">Popular Nerd</h1>
+  <nav>
+    <a href="#">About</a>
+    <a href="#">Portfolio</a>
+    <a href="#" class="last">Contact</a>
+  </nav>
+</header>
