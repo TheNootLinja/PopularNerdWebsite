69d458445f577770b894a273c1a8b70cc7b0354d
TheNootLinja <thenootlinja@gmail.com>
2020-12-15 22:46:46 -0500

    Add NavMenu and PopUpMenu components, Add hamburger menu icon to header when screen is mobile width, Add code for click on hb menu button to do a simple animation and open up the PopUpMenu component.

--- src/UI/Header.svelte
+++ src/UI/Header.svelte
@@ -1,31 +1,59 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let hbOpen = false;
+
+  function handleHbClick() {
+    dispatch("openmenu");
+    hbOpen = !hbOpen;
+  }
 </script>
 
 <style>
-  /* header {
-    color: white;
-    position: fixed;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 3rem;
-    background-color: #333;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  } */
-
-  /* header nav a {
-    color: white;
-    text-decoration: none;
-    border-bottom: solid 2px #00ebc7;
-  } */
-
   header nav a + a {
     margin-left: 10px;
   }
 
+  /* =========================================================== */
+
+  a {
+    display: none;
+  }
+
+  .container {
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  .bar1,
+  .bar2,
+  .bar3 {
+    width: 35px;
+    height: 5px;
+    background-color: #90a9b7;
+    margin: 6px 0;
+    transition: 0.4s;
+  }
+
+  /* Rotate first bar */
+  .change .bar1 {
+    -webkit-transform: rotate(-45deg) translate(-9px, 6px);
+    transform: rotate(-45deg) translate(-9px, 6px);
+  }
+
+  /* Fade out the second bar */
+  .change .bar2 {
+    opacity: 0;
+  }
+
+  /* Rotate last bar */
+  .change .bar3 {
+    -webkit-transform: rotate(45deg) translate(-8px, -8px);
+    transform: rotate(45deg) translate(-8px, -8px);
+  }
+  /* =========================================================== */
   .last {
     margin-right: 0.5rem;
   }
@@ -37,59 +65,12 @@
     padding-right: 6px;
     color: #90a9b7;
   }
-
-  .hb {
-    width: 35px;
-    height: 5px;
-    background-color: #00ebc7;
-    margin: 6px 20px;
-    display: none;
-  }
-
-  .hb-container {
-    display: none;
-    height: 35px;
-    width: fit-content;
-  }
-  /* ==================================================== */
-  .nav-icon {
-    margin: 1em;
-    width: 40px;
-  }
-
-  .nav-icon:after,
-  .nav-icon:before,
-  .nav-icon div {
-    background-color: #fae100;
-    border-radius: 3px;
-    content: "";
-    display: block;
-    height: 5px;
-    margin: 7px 0;
-    transition: all 0.2s ease-in-out;
-  }
-
-  .nav-icon:hover:before {
-    transform: translateY(12px) rotate(135deg);
-  }
-
-  .nav-icon:hover:after {
-    transform: translateY(-12px) rotate(-135deg);
-  }
-
-  .nav-icon:hover div {
-    transform: scale(0);
-  }
-  /* =================================================== */
-  @media only screen and (max-width: 500px) {
+  @media only screen and (min-width: 500px) {
     a {
-      /* display: none; */
+      display: inline-block;
     }
-    .hb {
-      display: block;
-    }
-    .hb-container {
-      display: block;
+    .container {
+      display: none;
     }
   }
 </style>
@@ -97,12 +78,14 @@
 <header>
   <h1 class="fake-logo">NRP</h1>
   <nav>
+    <div class="container {hbOpen ? 'change' : ''}" on:click={handleHbClick}>
+      <div class="bar1" />
+      <div class="bar2" />
+      <div class="bar3" />
+    </div>
     <a href="#aboutme">About</a>
     <a href="#skills">Skills</a>
     <a href="#projectgrid">Projects</a>
     <a href="#contact" class="last">Contact</a>
-    <!-- <div class="nav-icon"> -->
-    <div />
-    <!-- </div> -->
   </nav>
 </header>
---
+++ src/UI/PopUpMenu.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<style>
+    .container {
+        margin-top: 3rem;
+        height: 300px;
+        width: 100%;
+        background-color: #333;
+        position: fixed;
+        z-index: 1;
+    }
+    ul {
+        padding: 0;
+        margin: auto;
+        margin-top: 2rem;
+        width: 50%;
+    }
+    li {
+        list-style: none;
+        text-align: center;
+    }
+    a {
+        text-decoration: none;
+    }
+</style>
+
+<div class="container">
+    <ul>
+        <li><a href="">About</a></li>
+        <li><a href="">Experience</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</div>
